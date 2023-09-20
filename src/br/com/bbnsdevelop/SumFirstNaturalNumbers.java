198de8d6bc555471803ad5247010b41632c3c420
bbnsdevelop <bbnsdevelop@gmail.com>
2023-09-19 21:45:39 -0300

    chore: sum of first natural numbers

---
+++ src/br/com/bbnsdevelop/SumFirstNaturalNumbers.java
@@ -0,0 +1,21 @@
+package br.com.bbnsdevelop;
+
+public class SumFirstNaturalNumbers {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+		int sum = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+
+		System.out.println(sum);
+		
+		
+		int sumOtherWay = n * (n + 1) / 2;		
+		System.out.println(sumOtherWay);
+	}
+
+}
